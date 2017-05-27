d3e65204722404a7aa7ee29b1e9a13b77fcaa7a4
tigerjack <tigerjack89@gmail.com>
2017-05-28 01:08:33 +0200

    Improved .pyx; present bug into random_level function

--- Cython/skiplist.pyx
+++ Cython/skiplist.pyx
@@ -1,132 +1,186 @@
-from random import random
+#from random import random
+from libc.limits cimport INT_MAX, INT_MIN
+from libc.stdlib cimport rand, RAND_MAX
+import logging
 
-class _Node:
+logger_file = 'skiplist.log'
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.DEBUG)
 
+#WARN: it overwrites the file every time
+fh = logging.FileHandler(logger_file, 'w')
+fh.setLevel(logging.DEBUG)
+ch = logging.StreamHandler()
+ch.setLevel(logging.ERROR)
+formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+fh.setFormatter(formatter)
+ch.setFormatter(formatter)
+logger.addHandler(fh)
+logger.addHandler(ch)
+
+  
+cdef class _Node:
+    cdef public int key, value
+    cdef public list forward
+     
     #Height start counting by 1
-    def __init__(self, key, value = 0, height = 0):
-        print ("Creating node with height ", str(height))
+    def __cinit__(self, int key, int value = 0, int height = 0):
+        logger.debug("Creating node with height " + str(height))
         self.key = key
         self.value = value
         self.forward = [0] * height
-        print("Printing forward", str(self.forward))
+        logger.debug("Printing forward" + str(self.forward))
                                     
-    def print_node(n):               
-        if isinstance(n, _Node):
-            print("Node --- k:", str(n.key), ", v:", str(n.value))
-        else:
-            print(str(n))
+    cpdef node_to_string(self):               
+        logger.debug("Converting node to string")
+        s = "--- Node ---  k:" + str(self.key)+ ", v:" + str(self.value)
+        return s
 
-    def print_node_with_adjacents(self):
-        self.print_node()
-        print("Adjacent element, level 0 to (excluded): " + str(len(self.forward)))
+    cpdef node_with_adjacents_to_string(self):
+        s = self.node_to_string()
+        logger.debug("Adjacent element, level 0 to (excluded): " + str(len(self.forward)))
+        s += "\n\t --- Adjacents ---"
         for n in self.forward:
-            n.print_node()
-        print("---End adjacent nodes")
+            s += "\n\t" + n.node_to_string()
+        return s
 
-
-class SkipList:
-
+cdef class SkipList:
+    cdef public int p, maxLevel, level
+    cdef _Node HEADER, NIL
+    
     #Note: level assumes counting notation starts from 0, maxLevel from 1
-    def __init__(self, p, maxLevel):
-        print("Initializing skip list")
-        self.INT_MIN = -1000
-        self.INT_MAX = 1000
+    def __cinit__(self, int p, int maxLevel = 1):
+        logger.debug("Initializing skip list")
+        if maxLevel < 1 or p < 0:
+            logger.error("Illegal values passed to constructor")
+            raise ValueError("Illegal constructor value")
+         
         self.p = p
         self.maxLevel = maxLevel
         self.level = 0
-        self.HEADER = _Node(self.INT_MIN, 0, 0)
-        self.NIL = _Node(self.INT_MAX, 100, 0)
+        self.HEADER = _Node(INT_MIN, 0, 0)
+
+        self.NIL = _Node(INT_MAX, 100, 0)
         # Note: we start from level 0 (not 1 as in the paper)
+        cdef int i 
         for i in range(maxLevel):
             self.HEADER.forward.append(self.NIL)
-        self.print_list()
+        logger.debug(self.list_to_string())
+        
 
-    def print_list(self):
-        print("SKIPLIST\n" + "Max level = " + str(self.maxLevel) + ", Level = " + str(self.level) + "\n")
+    # Bugged: it always return 0
+    # Note: must use C random for performance. However, even using python
+    # random nothing changes
+    cpdef list_to_string(self):
+        logger.debug("Converting list to string")
+        s = "--- Skiplist --- \n" + "Max level = " + str(self.maxLevel) \
+            + ", Level = " + str(self.level) + ", p = " + str(self.p)
+        cdef int i
         for i in range(self.maxLevel): 
-            self.print_level(i)
+            s += "\n\t" +  self.level_to_string(i)
+        logger.debug("Ending conversion")
+        return s
 
-    def print_level(self, l):
+    cpdef level_to_string(self, int l):
         if l >= self.maxLevel:
             raise ValueError("Level greater than MaxValue")
 
-        print("Printing Level = " + str(l))
-        y = self.HEADER
-        y.print_node()
+        logger.debug("Printing Level = " + str(l))
+        s = "--- Level " + str(l) + " ---"
+        cdef _Node y = self.HEADER
+        s += "\n\t" + y.node_to_string()
         while y.forward[l] != self.NIL:
-            y.forward[l].print_node()
+            s += "\n\t" + y.forward[l].node_to_string()
             y = y.forward[l]
-        y.forward[l].print_node()
-        print("---End of Level---")
+        s += "\n\t" + y.forward[l].node_to_string()
+        return s
+
+    cpdef int _random_level(self):
+        cdef int level = 0
+        cdef double random = rand() / RAND_MAX
+        # int(rand()/(RAND_MAX100.0)) # random int 1..100
+        # (double) (rand() / RAND_MAX)
+        print("r=" + str(random))
          
-    def _random_level(self):
-        level = 0
-        while random() < self.p and level < self.maxLevel - 1:
+        while random < self.p and level < self.maxLevel - 1:
             level += 1
         return level
              
-    def _update_list(self, key):
-        update = [None] * (self.level + 1)
-        x = self.HEADER
+    cdef list _update_list(self, int key):
+        cdef list update = [None] * (self.level + 1)
+        cdef _Node x = self.HEADER
+        cdef int i
         for i in range(self.level, -1, -1):
             while x.forward[i].key < key:
-                print("Inside while")
                 x = x.forward[i]
             update[i] = x
-        print("***Printing update list")
+             
+        logger.debug("Printing update list")
         for n in update:
-            n.print_node_with_adjacents()
-        print("***End update list")
+            logger.debug(n.node_with_adjacents_to_string())
+        logger.debug("End update list")
+             
         return update
+
+    cpdef list get_nodes_of_level(self, int l):
+        if l >= self.maxLevel:
+            raise ValueError("Level greater than MaxValue")
+
+        cdef list nodes = []
+        cdef _Node y = self.HEADER
+        while y.forward[l] != self.NIL:
+            nodes.append(y)
+            y = y.forward[l]
+        nodes.append(y)
+        nodes.append(self.NIL)
+        return nodes
          
-    def insert(self, key, value):
-        if key <= self.INT_MIN or key >= self.INT_MAX:
+    cpdef bint insert(self, int key, int value):
+        logger.debug("Trying to insert new node, k %s, v %s", key, value)
+        if key <= INT_MIN or key >= self.INT_MAX:
+            logger.error("Illegal key value passed to insert")
             raise ValueError("Illegal key value")
         
-        update = self._update_list(key)
-        # if update[0].forward[0].key != self.NIL.key:
-        #     x = update[0].forward[0]
-        # else:
-        #     print("Inserting first node")
-        #     x = self.HEADER
-        x = update[0].forward[0]
-             
-        print("Level 0 predecessor: ")
-        x.print_node()
+        cdef list update = self._update_list(key)
+        cdef _Node x = update[0].forward[0]
+        logger.debug("Candidate: " + x.node_to_string())
+         
+        cdef int lvl = self._random_level()
+        cdef int i
          
         if x.key == key:
-            print("Key already present, updating value")
+            logger.debug("Key already present, updating value")
             x.value = value
         else:
-            lvl = self._random_level()
-            print("Random level selected " + str(lvl))
+            logger.debug("Random level selected " + str(lvl))
             x = _Node(key, value, lvl + 1)
+            logger.debug(x.node_to_string())
     
             if lvl > self.level:
+                logger.debug("Level > current level")
+                i = 0
                 for i in range(self.level + 1, lvl + 1):
                     update.append(self.HEADER)
                 self.level = lvl
-                print("***Printing update list")
+                logger.debug("Printing update list")
                 for n in update:
-                    n.print_node_with_adjacents()
-                print("***End update list")
+                    logger.debug(n.node_with_adjacents_to_string())
+                logger.debug("End update list")
+            i = 0
+            for i in range (lvl + 1):
+                try:
+                    logger.debug("Iteration " + str(i))
+                    x.forward[i] = update[i].forward[i]
+                    logger.debug("Node appended")
+                    update[i].forward[i] = x
+                    logger.debug("Forward node updated")
+                except IndexError as err:
+                    logger.error("Error while inserting node at iteration %d", i)
+                    logger.error(x.node_with_adjacents_to_string())
+                    logger.error(update[i].node_with_adjacents_to_string())
+                    logger.error(update[i].forward[i].node_with_adjacents_to_string())
+                    return False
+                
+            logger.debug(self.list_to_string())
+            return True
              
-            for i in range (self.level + 1):
-                print("Iteration " + str(i))
-                # print("update[i].forward[i] node before append")
-                # update[i].forward[i].print_node_with_adjacents()
-                x.forward[i] = update[i].forward[i]
-                print("Node appended")
-                # x.print_node_with_adjacents()
-                 
-                # print("update[i].forward[i] node after append, before update")
-                # update[i].forward[i].print_node_with_adjacents()
-                update[i].forward[i] = x
-                print("Forward node updated")
-            
-            x.print_node_with_adjacents()
-
-            # print("printing update list")
-            # for n in update:
-            #     n.print_node_with_adjacents()
-                 
